899ba5358fac313d03004d23549d3d954a42ba8b
jd1705 <soulbusterjd17@gmail.com>
2026-05-01 12:41:54 -0400

    create request command

--- plugin/adore.lua
+++ plugin/adore.lua
@@ -13,3 +13,14 @@ end, { desc = "Unfocus the cursor from the client and focus on the editor window
 vim.api.nvim_create_user_command("AdoRestMessage", function ()
     require('adore').world_domination()
 end, { desc = "Print a message"})
+
+vim.api.nvim_create_user_command("AdoRestRequest", function ()
+    local adorest = require("adore")
+    local lines = vim.api.nvim_buf_get_lines(adorest.ui.buf_url, 0, -1, false)
+    local request = adorest.get_data(lines)
+    if request.url == "" then
+        print("AdoRest: Error - URL is empty!")
+        return
+    end
+    adorest.execute_request(request.method, request.url, request.body, request.header, request.query)
+end, { desc = "Send a request if the bar is open"})
